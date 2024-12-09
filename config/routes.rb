e5c05932cdc5ca4554c72617018e3a475f0ffd36
AMT98 <aswinmalla12@gmail.com>
2024-12-09 14:02:32 -0700

    Db migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show]
   resources :tasks
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
